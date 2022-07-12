ad216d2f4251e7f27cc95d1a0a7dec4590d35108
nicboi1 <nboie001@ucr.edu>
2022-07-11 20:42:42 -0700

    first check minor

--- phong_shader.cpp
+++ phong_shader.cpp
@@ -12,17 +12,20 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     TODO; //determine the color
 
    for(int i=0; i<world.lights.size();i++){
+       //light
        vec3 l=world.lights.at(i)->position-intersection_point;
+       //view
        vec3 v=ray.direction;
-       vec3 r = ((2* dot(v,normal))*normal-v).normalized();
+       //reflect
+       vec3 r=((2*dot(v,normal))*normal-v).normalized();
         //R=K, L=intensity
        //emitted light->intensity
        for(int j=0;j<3;j++){
            double LDS=world.lights.at(i)->Emitted_Light(l)[j];
            double firMax=fmax(dot(normal,l.normalized()),0);
            double secMax=fmax(dot(v,r),0);
-
-           color[j]+=color_ambient[j]*world.ambient_color[j]*world.ambient_intensity+color_diffuse[j]*LDS*firMax+color_specular[j]*LDS*pow(secMax,specular_power);
+            //ca
+           color[j]+=world.ambient_color[j]*world.ambient_intensity+color_diffuse[j]*LDS*firMax+color_specular[j]*LDS*pow(secMax,specular_power);
 
        }
 
