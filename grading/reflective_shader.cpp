056643ea1e8dcb0e3682b246e4f530cfc5421798
nicboi1 <nboie001@ucr.edu>
2022-07-23 16:49:04 -0700

    almost final

--- grading/reflective_shader.cpp
+++ grading/reflective_shader.cpp
@@ -6,7 +6,25 @@ vec3 Reflective_Shader::
 Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& normal,int recursion_depth) const
 {
+
     vec3 color;
-    TODO; // determine the color
-    return color;
+    //percent of reflected light from objects that shows,fully relflective obj=no color
+    color = (1-reflectivity)*shader->Shade_Surface(ray,intersection_point,normal,recursion_depth);
+    //shade surface->phong shader color
+    vec3 d = ray.direction;
+    vec3 n = normal;
+    vec3 C = d+(-2.0*(dot(d,n)*n));
+    Ray rayC(intersection_point,C);
+
+   if(recursion_depth==world.recursion_depth_limit){
+       return color;
+   }else{
+       recursion_depth++;
+       //c+km*phong shader color
+       return color+(reflectivity*world.Cast_Ray(rayC,recursion_depth));
+
+
+   }
+
+
 }
