9070525a0a5b1cd3d6cd8995616f0f2c904859d3
nicboi1 <nboie001@ucr.edu>
2022-07-25 16:17:38 -0700

    reflective shades

--- reflective_shader.cpp
+++ reflective_shader.cpp
@@ -8,21 +8,24 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
 {
 
     vec3 color;
-    //percent of reflected light from objects that shows,fully relflective obj=no color
-    color = (1-reflectivity)*shader->Shade_Surface(ray,intersection_point,normal,recursion_depth);
     //shade surface->phong shader color
-    vec3 d = ray.direction;
+    vec3 d = -ray.direction;
     vec3 n = normal;
-    vec3 C = d+(-2.0*(dot(d,n)*n));
+    vec3 C = (2.0*dot(d,n)*n-(d)).normalized();
     Ray rayC(intersection_point,C);
-
-   if(recursion_depth==world.recursion_depth_limit){
-       return color;
-   }else{
+    //og color
+    vec3 SS=shader->Shade_Surface(ray,intersection_point,normal,recursion_depth);
+    color=SS;
+    //color=Co+reflectivity(Cr-Co);
+   if(recursion_depth!=world.recursion_depth_limit){
        recursion_depth++;
-       return color+(reflectivity*world.Cast_Ray(rayC,recursion_depth));
-
-
+       //other color
+       vec3 SSR=world.Cast_Ray(rayC,recursion_depth);
+       color+=(reflectivity*(SSR-SS));
+       return color;
+   }else {
+       color+=(reflectivity * (-SS));
+       return color;
    }
 
 
